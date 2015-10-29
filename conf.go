b43be17c8f040f8d7b4ef38b9e260b87862bfad4
eycia <zhou.eycia@gmail.com>
2015-10-29 12:45:52 +0800

    init

---
+++ conf.go
@@ -0,0 +1,14 @@
+package config
+
+
+func LoadConfDir(_struct interface{}, confdir string) error {
+
+}
+
+func LoadConfFromJson(_struct interface{}, conf map[string]interface{}) error {
+
+}
+
+func loadConfFromJson(_struct interface{}, conf map[string]interface{}) error {
+
+}
