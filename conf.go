b51e1f58ee01aee5a18baad41b499a9156653c99
Eycia Zhou <zhou.eycia@gmail.com>
2016-02-16 22:50:41 +0800

    switch to linux, finished load from defualt and flag

--- conf.go
+++ conf.go
@@ -2,15 +2,15 @@ package configparser
 
 import (
 	"reflect"
-	//"fmt"
 	"errors"
-	//"strings"
 	"strconv"
 	"fmt"
 	"encoding/json"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"flag"
+	"time"
 )
 
 var (
@@ -71,7 +71,7 @@ func LoadConfPath(_struct interface{}, confpath string) error {
 	return LoadConfFromJson(_struct, v)
 }
 
-func LoadConfFromJson(_struct interface{}, conf map[string]interface{}) error {
+func checkTypeAndPanic(_struct interface{}) (reflect.Value, reflect.Type) {
 	if reflect.TypeOf(_struct).Kind() != reflect.Ptr {
 		panic(ErrorNotStruct)
 	}
@@ -83,12 +83,217 @@ func LoadConfFromJson(_struct interface{}, conf map[string]interface{}) error {
 		panic(ErrorNotStruct)
 	}
 
+	return v, t
+}
+
+func LoadConfFromFlag(_struct interface{}) {
+	v, t := checkTypeAndPanic(_struct)
+
 	for i := 0; i < v.NumField(); i++ {
 		fieldName := t.Field(i).Name
-		//fieldName := strings.ToLower(t.Field(i).Name)
 
 		if !v.Field(i).CanSet() {
 			panic(fmt.Errorf("In field %s, unexported field", fieldName))
+		}
+
+
+	}
+}
+
+type sliceValue struct {
+	typ reflect.Type
+	vals reflect.Value
+	setted bool
+	field string
+}
+
+func (s *sliceValue) String() string {
+	return fmt.Sprintf("%v", s.vals)
+}
+
+func (sl *sliceValue) Set(s string) error {
+	sl.setted = true
+
+	var tmpv interface{}
+
+	if s == "" {
+		tmpv = []interface{}{}
+	} else {
+		err := json.Unmarshal(([]byte)(s), &tmpv)
+		if err != nil {
+			return fmt.Errorf("In field %s, unmarshal slice error : %s", sl.field, err.Error())
+		}
+	}
+
+	vm := reflect.ValueOf(tmpv)
+
+	tshould := sl.typ
+
+	le := vm.Len()
+	tmpTransed := reflect.MakeSlice(reflect.SliceOf(tshould), le, le)
+
+	for i := 0; i < le; i++ {
+		if reflect.TypeOf(vm.Index(i).Interface()).ConvertibleTo(tshould) {
+			tmpTransed.Index(i).Set( reflect.ValueOf(vm.Index(i).Interface()).Convert(tshould) )
+		} else {
+			return fmt.Errorf("In field %s, have a Un Convertible value, at postion %d", sl.field, i)
+		}
+	}
+
+	sl.vals = tmpTransed
+
+	return nil
+}
+
+func addFlagMap(_struct interface{}) error {
+	if flag.Parsed() {
+		return errors.New("error when add flag arguments: parsed flags cann't parse twice")
+	}
+
+	v, t := checkTypeAndPanic(_struct)
+
+	slicePostion := []int{}
+	sliceValues := []*sliceValue{}
+ 	for i := 0; i < v.NumField(); i++ {
+		fieldName := t.Field(i).Name
+
+		if !v.Field(i).CanSet() {
+			continue;
+		}
+
+		pointer := v.Field(i).Addr().Interface()
+
+		usage := t.Field(i).Tag.Get("usage")
+
+		switch t.Field(i).Type.Kind() {
+
+		case reflect.String:
+			flag.StringVar(pointer.(*string), fieldName, v.Field(i).String(), usage)
+
+		case reflect.Bool:
+			flag.BoolVar(pointer.(*bool), fieldName, v.Field(i).Bool(), usage)
+
+		case reflect.Int:
+			flag.IntVar(pointer.(*int), fieldName, (int)(v.Field(i).Int()), usage)
+
+		case reflect.Int64:
+			if t.Field(i).Type == reflect.TypeOf(time.Second) {
+				flag.DurationVar(pointer.(*time.Duration), fieldName,
+					time.Duration(v.Field(i).Int()), usage)
+			} else {
+				flag.Int64Var(pointer.(*int64), fieldName, v.Field(i).Int(), usage)
+			}
+
+		case reflect.Float64:
+			flag.Float64Var(pointer.(*float64), fieldName, v.Field(i).Float(), usage)
+
+		case reflect.Slice:
+			slicePostion = append(slicePostion, i)
+			sliceValues = append(sliceValues, &sliceValue{
+				typ: t.Field(i).Type.Elem(),
+				field: fieldName,
+			})
+
+			flag.Var(sliceValues[len(sliceValues)-1], fieldName, usage)
+
+		default:
+			panic(fmt.Errorf("In field %s, unsupported type", fieldName))
+		}
+	}
+
+	return nil
+}
+
+func getDefaultMap(_struct interface{}) error {
+	v, t := checkTypeAndPanic(_struct)
+
+	for i := 0; i < v.NumField(); i++ {
+		fieldName := t.Field(i).Name
+
+		if !v.Field(i).CanSet() {
+			continue
+		}
+
+		switch raw := t.Field(i).Tag.Get("default"); t.Field(i).Type.Kind() {
+		case reflect.String:
+			v.Field(i).SetString(raw)
+		case reflect.Bool:
+			if raw == "true" || raw == "TRUE" || raw == "True" || raw == "T" || raw == "t" || raw == "1" {
+				v.Field(i).SetBool(true)
+			} else if raw == "" || raw == "false" || raw == "FALSE" || raw == "False" || raw == "F" || raw == "f" || raw == "0" {
+				v.Field(i).SetBool(false)
+			} else {
+				return fmt.Errorf("In field %s, default string can't parse to bool", fieldName)
+			}
+
+		case reflect.Int64, reflect.Int:
+			if t.Field(i).Type == reflect.TypeOf(time.Duration(0)) {
+				//if is time duration, progress by time.Duration
+
+				dur, err := time.ParseDuration(raw)
+				if err != nil {
+					return fmt.Errorf("In field %s, default string can't parse to time.Duration", fieldName)
+				}
+				v.Field(i).SetInt((int64)(dur))
+			} else {
+				if raw == "" {
+					v.Field(i).SetInt(0)
+					continue
+				}
+
+				tmpv, e := strconv.ParseInt(raw, 10, 64)
+
+				if e != nil {
+					return fmt.Errorf("In field %s, default string can't be parse to int", fieldName)
+				}
+
+				v.Field(i).SetInt(tmpv)
+			}
+
+		case reflect.Float64:
+
+			if raw == "" {
+				v.Field(i).SetFloat(0.0)
+				continue
+			}
+
+			tmpv, e := strconv.ParseFloat(t.Field(i).Tag.Get("default"), 64)
+
+			if e != nil {
+				return fmt.Errorf("In field %s, default string can't be parse to float", fieldName)
+			}
+
+			v.Field(i).SetFloat(tmpv)
+
+		case reflect.Slice:
+			sl := &sliceValue{
+				typ:t.Field(i).Type.Elem(),
+				field:fieldName,
+			}
+
+			if e := sl.Set(raw); e != nil {
+				return e
+			}
+
+			v.Field(i).Set(sl.vals)
+
+		default:
+			panic(fmt.Errorf("In field %s, unsupported type", fieldName))
+		}
+	}
+
+	return nil
+}
+
+func LoadConfFromJson(_struct interface{}, conf map[string]interface{}) error {
+	v, t := checkTypeAndPanic(_struct)
+
+	for i := 0; i < v.NumField(); i++ {
+		fieldName := t.Field(i).Name
+
+		if !v.Field(i).CanSet() {
+			continue
+			//panic(fmt.Errorf("In field %s, unexported field", fieldName))
 		}
 
 		switch mv, ok := conf[fieldName]; t.Field(i).Type.Kind() {
@@ -106,9 +311,9 @@ func LoadConfFromJson(_struct interface{}, conf map[string]interface{}) error {
 
 			if !ok {
 				raw := t.Field(i).Tag.Get("default")
-				if raw == "true" || raw == "True" || raw == "T" || raw == "1" {
+				if raw == "true" || raw == "TRUE" || raw == "True" || raw == "T" || raw == "t" || raw == "1" {
 					v.Field(i).SetBool(true)
-				} else if raw == "false" || raw == "False" || raw == "F" || raw == "0" {
+				} else if raw == "false" || raw == "FALSE" || raw == "False" || raw == "F" || raw == "f" || raw == "0" {
 					v.Field(i).SetBool(false)
 				} else {
 					return fmt.Errorf("In field %s, default string can't parse to bool", fieldName)
@@ -214,13 +419,33 @@ func LoadConfFromJson(_struct interface{}, conf map[string]interface{}) error {
 	return nil
 }
 
-
 func AutoLoadConfig(moduleName string, _struct interface{}) error {
 	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 
 	err := LoadConfPath(_struct, dir + "/conf/" + moduleName + ".conf")
-	if err != nil {
-		return LoadConfDefault(_struct)
+	if err == nil {
+		return nil
 	}
-	return nil
+
+	err = LoadConfPath(_struct, dir + "/conf/" + moduleName + ".json")
+	if err == nil {
+		return nil
+	}
+
+	err = LoadConfPath(_struct, dir + moduleName + ".conf")
+	if err == nil {
+		return nil
+	}
+
+	err = LoadConfPath(_struct, dir + moduleName + ".json")
+	if err == nil {
+		return nil
+	}
+
+	err = LoadConfPath(_struct, dir + moduleName + "conf.json")
+	if err == nil {
+		return nil
+	}
+
+	return LoadConfDefault(_struct)
 }
